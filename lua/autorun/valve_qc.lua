a1ae76f17c17c6b7bc20f48c9f0867859a5cfb98
Cpt-Hazama <Caleb123459@gmail.com>
2022-07-01 15:31:28 -0400

    Updates + Additions
    - Added Valve.GenerateSMDFile(fileName, tbl)
    - Updated parameters for Valve.CreateSequences(smds,defArgs,fileName,findInDir)
    
    - Added a proper exporter for $Sequences (see first listed function)
    - Added the ability to search directories for SMDs to automatically create a data table
    - Added a new defArg argument, Auto, which will automatically try to setup $Sequence parameters

--- lua/autorun/valve_qc.lua
+++ lua/autorun/valve_qc.lua
@@ -1,4 +1,87 @@
 Valve = Valve or {}
+
+local table_insert = table.insert
+local table_remove = table.remove
+
+local string_find = string.find
+local string_lower = string.lower
+local string_sub = string.sub
+local string_Replace = string.Replace
+--
+--------------------------------------------------------------------------------------------------------------------------------------------
+--[[---------------------------------------------------------
+	Generates a QC txt file for Sequences, should be ran through Valve.CreateSequences rather than just by itself
+		- name = The generated file name
+		- tbl = The SMD data
+    Example
+        local tbl = {
+            {
+                smd = "idle",
+                fps = 30,
+                loop = true,
+                walkframes = 0
+            }
+            {
+                smd = "walk",
+                fps = 30,
+                loop = true,
+                walkframes = 35
+            }
+        }
+        Valve.GenerateSMDFile( "zombie", tbl )
+-----------------------------------------------------------]]
+Valve.GenerateSMDFile = function(fileName,tbl)
+    file.CreateDir("valve/smd")
+
+    local function AddSequence(f,smdDat,isFirst)
+        local smd = smdDat.smd
+        local addLoop = smdDat.loop or false
+        local setFPS = smdDat.fps or 30
+        local walkframes = smdDat.walkframes or false
+
+        print("SMD MODEL " .. smd .. ".smd")
+        if !isFirst then
+            f:Write("\n")
+        end
+        f:Write("\n")
+        f:Write('$Sequence "' .. smd .. '" {')
+            f:Write("\n")
+            f:Write('\t "animations/' .. smd .. '.smd"')
+            f:Write("\n")
+            f:Write('\t activity "ACT_' .. smd .. '" 1')
+            if setFPS then
+                f:Write("\n")
+                f:Write('\t fps ' .. setFPS)
+            end
+            if addLoop then
+                f:Write("\n")
+                f:Write('\t loop')
+            end
+            if walkframes then
+                f:Write("\n")
+                f:Write('\t walkframe ' .. walkframes .. ' LX LY')
+                print("@" .. smd .. " : 0 - " .. walkframes .. "")
+            end
+            f:Write("\n")
+        f:Write('}')
+    end
+
+    local f = file.Open("valve/smd/" .. fileName .. ".qc.txt","w","DATA")
+        print("Compiling '" .. fileName .. ".txt' ...")
+        f:Write("// Compiled using Valve Lua Tools")
+        f:Write("\n")
+        for i,v in pairs(tbl) do
+            AddSequence(f,v,i == 1)
+        end
+    f:Close()
+    local f = file.Open("valve/smd/" .. fileName .. ".qc.txt","r","DATA")
+        local bytes = f:Size()
+        print("sequences     " .. bytes .. " bytes (" .. #tbl .. " seq)")
+        print("Completed '/common/GarrysMod/garrysmod/data/valve/smd/" .. fileName .. ".qc.txt'")
+    f:Close()
+
+    print('Note: If you do not want to manually add the sequences to the QC file, you can simply copy the file to the QC folder, remove the .TXT extension, and add $include "' .. fileName .. '.qc" to the original QC file.')
+end
 --------------------------------------------------------------------------------------------------------------------------------------------
 --[[---------------------------------------------------------
 	Generates a SMD txt file from a list of animation names (use 'dir /s /b /o:gn' to get the list via cmd)
@@ -36,33 +119,67 @@ end
 	Generates and prints a list of $sequences to the console in QC format
 		- smds = The list of animation names or the SMD txt file name
         - defArgs = The default arguments to use for the sequences
+        - fileName = If set, it will generate a QC file for the sequences with this name
+        - findInDir = If set to true, it will search in the smds directory for .SMD files and use them
     Example
         local tbl = { "anim1", "anim2", "anim3" }
-        Valve.CreateSequences( "anims")
+        Valve.CreateSequences( "anims", {Auto = true}, "SMD_List", true)
+        Valve.CreateSequences( "anims", {FPS = 60, WalkFrames = 900})
         Valve.CreateSequences( tbl )
 -----------------------------------------------------------]]
-Valve.CreateSequences = function(smds,defArgs)
+Valve.CreateSequences = function(smds,defArgs,fileName,findInDir)
     if type(smds) != "table" then
-        smds = Valve.ReadSMDs(smds)
+        if findInDir then
+            smds = file.Find("valve/smd/" .. smds .. "/*.smd","DATA")
+        else
+            smds = Valve.ReadSMDs(smds)
+        end
+    end
+    if smds == nil or istable(smds) && #smds <= 0 then
+        print("No SMD files found ...")
+        return
     end
     local list = {}
+    local auto = defArgs.Automatic or false
     local addLoop = defArgs.Loop or false
     local setFPS = defArgs.FPS or 30
     local walkframes = defArgs.WalkFrames or false
     for _,smd in pairs(smds) do
-        print("\n")
-        print('$Sequence "' .. smd .. '" {')
-            print('\t "animations/' .. smd .. '.smd"')
-            print('\t activity "ACT_' .. smd .. '" 1')
-            if setFPS then
-                print('\t fps ' .. setFPS)
+        if string_find(smd,".smd") then
+            smd = string_sub(smd,1,-5)
+        end
+        if auto then
+            local smdName = string_lower(smd)
+            if string_find(smdName,"idle") or string_find(smdName,"wait") or string_find(smdName,"walk") or string_find(smdName,"run") or string_find(smdName,"all") or string_find(smdName,"glide") or string_find(smdName,"loop") then
+                addLoop = true
+            elseif string_find(smdName,"walk") or string_find(smdName,"run") or string_find(smdName,"attack") or string_find(smdName,"range") or string_find(smdName,"jump") or string_find(smdName,"land") then
+                walkframes = 900
             end
-            if addLoop then
-                print('\t loop')
-            end
-            if walkframes then
-                print('\t walkframe ' .. walkframes .. ' LX LY')
-            end
-        print('}')
+        end
+        if !fileName then
+            print("\n")
+            print('$Sequence "' .. smd .. '" {')
+                print('\t "animations/' .. smd .. '.smd"')
+                print('\t activity "ACT_' .. smd .. '" 1')
+                if setFPS then
+                    print('\t fps ' .. setFPS)
+                end
+                if addLoop then
+                    print('\t loop')
+                end
+                if walkframes then
+                    print('\t walkframe ' .. walkframes .. ' LX LY')
+                end
+            print('}')
+        end
+        list[#list +1] = {
+            smd = smd,
+            fps = setFPS,
+            loop = addLoop,
+            walkframes = walkframes
+        }
+    end
+    if fileName then
+        Valve.GenerateSMDFile(fileName,list)
     end
 end
